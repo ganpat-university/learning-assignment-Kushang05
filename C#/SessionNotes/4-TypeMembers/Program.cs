e5cb67a72e1f107624f235118b93dcf7baa72ccc
Kushang05 <98147091+Kushang05@users.noreply.github.com>
2022-02-09 11:30:35 +0530

    Add files via upload

---
+++ C#/SessionNotes/4-TypeMembers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace 5-Array
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
