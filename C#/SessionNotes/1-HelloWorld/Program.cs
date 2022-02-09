09cdddfa57c87cb0578072c14ee70cfc18e9dc2e
Kushang05 <98147091+Kushang05@users.noreply.github.com>
2022-02-09 11:28:00 +0530

    Add files via upload

---
+++ C#/SessionNotes/1-HelloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
