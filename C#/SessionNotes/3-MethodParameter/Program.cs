947917ba89a6974e0ce8af437114194e9fd03f89
Kushang05 <98147091+Kushang05@users.noreply.github.com>
2022-02-09 11:29:25 +0530

    Add files via upload

---
+++ C#/SessionNotes/3-MethodParameter/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _3_MethodParameter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+
+            Console.WriteLine("Pass By Value");
+            a = 20;
+            Console.WriteLine(a);
+            PassByValue(a);
+            Console.WriteLine("a={0}", a);
+
+
+
+            Console.WriteLine("Pass By Refrence");
+            a = 10;
+            PassByRefrence(ref a);
+            Console.WriteLine("a={0}",a);
+
+            Console.WriteLine("Pass By Out");
+            a = 10;
+            PassByOut(out a);
+            Console.WriteLine("a={0}", a);
+
+        }
+
+        static void PassByValue( int a)
+        {
+            a = 100;
+            Console.WriteLine("after passing a={0]", a);
+        }
+
+        static void PassByRefrence(ref int a)
+        {
+            a = 200;
+            Console.WriteLine("after passing a={0]", a);
+        }
+        static void PassByOut(out int a)
+        {
+            a = 300;
+            Console.WriteLine("after passing a={0]", a);
+        }
+
+
+    }
+}
