459a92015940a1e10e203e9ba54834dea377ab2f
Kushang05 <98147091+Kushang05@users.noreply.github.com>
2022-02-09 11:28:46 +0530

    Add files via upload

---
+++ C#/SessionNotes/2-DataTypes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _2_DataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            bool b = true;
+            string s = "hello world";
+            char c = 'A';
+
+            int k = int.MaxValue;
+            a = k + 1;
+
+            long p= k;                 // implicit type-casting int value to long.
+
+            p = long.MaxValue;
+            
+            k = (int)p;                 // explicit type-casting long value to int
+
+            Console.WriteLine("l (long) = {0}, j (int) = {1}", p, k);
+
+
+        }
+    }
+    }
+}
