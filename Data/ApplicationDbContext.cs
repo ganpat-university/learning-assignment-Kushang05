bab9e8fd535e9cf18c5f5d42a49bb43d178b9b3d
Kushang05 <98147091+Kushang05@users.noreply.github.com>
2022-02-21 11:24:04 +0530

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project2.Models;
+
+
+namespace Project2.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Department> Departments { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
